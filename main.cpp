c0d9303cf9c8c5053db2b0ca95755fa6857272ff
Fane264 <nastasestefan2004@gmail.com>
2024-11-04 19:43:22 +0200

    v0.1

--- main.cpp
+++ main.cpp
@@ -6,19 +6,19 @@ int main() {
     std::cout <<"Starea initiala a tablei de joc"<< std::endl;
     board.displayBoard();
 
-    std::cout <<"Mut piesa 0 a primului jucator cu 3 pasi"<< std::endl;
+    std::cout <<"\nMut piesa 0 a primului jucator cu 3 pasi\n"<< std::endl;
     board.movePlayerPiece(0, 0, 3);
     board.displayBoard();
 
-    std::cout <<"Mut piesa 1 a celui de-al doilea jucator cu 5 pasi"<< std::endl;
+    std::cout <<"\nMut piesa 1 a celui de-al doilea jucator cu 5 pasi\n"<< std::endl;
     board.movePlayerPiece(1, 2, 5);
     board.displayBoard();
 
-    std::cout <<"Mut piesa 0 a primului jucator inapoi la start"<< std::endl;
+    std::cout <<"\nMut piesa 0 a primului jucator inapoi la start\n"<< std::endl;
     board.movePlayerPiece(0, 0, -3);
     board.displayBoard();
 
-    std::cout << "Mut piesa 4 a primului jucator cu 4 pasi"<< std::endl;
+    std::cout << "\nMut piesa 4 a primului jucator cu 4 pasi\n"<< std::endl;
     board.movePlayerPiece(0, 3, 4);
     board.displayBoard();
 
@@ -30,7 +30,7 @@ int main() {
     board.movePlayerPiece(1,3,7);
     board.displayBoard();
 
-    std::cout <<"Starea finala a tablei de joc";
+    std::cout <<"\nStarea finala a tablei de joc\n"<< std::endl;
     board.displayBoard();
 
     return 0;
